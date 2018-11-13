6e8acb3495e324f064ed6d1da4cbd44d8f6d9a1e
mike929 <mike929@gmail.com>
2018-11-13 01:01:39 -0500

    newupdate3

--- action_page.php
+++ action_page.php
@@ -470,7 +470,7 @@ Last name: <input type="text" name="LastName" value="Mouse"><br>
 <input type="submit" value="Submit">
 </form>
 
-<p>Click the "Submit" button and the form-data will be sent to a page on the server called &quot;/action_page.php&quot;.</p>
+<p>Click the "Submit" button and the form-data will be sent to a page on the server called action_page.php/action_page.php&quot;.</p>
 
 </body>
 </html>
